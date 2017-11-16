eb0c60ccfc9bc1472dffb8cb583e78b3dd36cd05
Cigaras <Cigaras@gmail.com>
2017-11-16 09:36:42 +0200

    Minor styling changes

--- More Letters/lua/multiprofileitemgui.lua
+++ More Letters/lua/multiprofileitemgui.lua
@@ -1,8 +1,3 @@
-Hooks:PostHook(                                 -- Automatically hooks a function to be called after the specified function on a specified object
-    MultiProfileItemGui,                        -- The object for the post-hook to search for the function to hook
-    "init",                                     -- A function name, as a string, on the object to be post-hooked
-    "MoreLettersMultiProfileItemGuiInit",       -- The unique identifier for this post-hook
-    function(self)                              -- The function that should be called after function is called on object
-        self._max_length = 30                   -- Number of letters for profile names, default value is 15
-    end
-)
+Hooks:PostHook(MultiProfileItemGui, "init", "MoreLettersMultiProfileItemGuiInit", function(self)
+	self._max_length = 30 -- number of letters for profile names, default value is 15
+end)
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Payday 2 More Letters
 [Payday 2](http://store.steampowered.com/app/218620/PAYDAY_2/) [BLT](https://github.com/JamesWilko/Payday-2-BLT) mod that doubles the amount of allowed input characters for renaming weapons, skill sets and profiles.
 
-Icon by [Custom Icon Design](http://www.customicondesign.com/), [free for non-commercial use](http://www.iconarchive.com/show/flatastic-7-icons-by-custom-icon-design/Letters-icon.html).
+Credits:
+* Author: [Valdas V.](https://valdasv.lt)
+* Icon by [Custom Icon Design](http://www.customicondesign.com/), [free for non-commercial use](http://www.iconarchive.com/show/flatastic-7-icons-by-custom-icon-design/Letters-icon.html).
