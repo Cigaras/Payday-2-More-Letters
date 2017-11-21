3ffd9d74667c7f514d7eeac704881edf33c9ca9b
Cigaras <Cigaras@gmail.com>
2017-11-21 08:40:12 +0200

    Minor styling changes

--- More Letters/lua/multiprofileitemgui.lua
+++ More Letters/lua/multiprofileitemgui.lua
@@ -1,3 +1,3 @@
-Hooks:PostHook(MultiProfileItemGui, "init", "MoreLettersMultiProfileItemGuiInit", function(self)
+Hooks:PostHook(MultiProfileItemGui, "init", "MultiProfileItemGui_init_MoreLetters", function(self)
 	self._max_length = 30 -- number of letters for profile names, default value is 15
 end)
